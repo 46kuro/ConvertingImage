88a281c25a46ac3f6e32ad31ef28bfc3f8285683
shinji46kuro <shinji.46.kuro@gmail.com>
2018-12-15 14:05:43 +0900

    Initial Commit

---
+++ ConvertingImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertingImage
+//
+//  Created by Shinji Kurosawa on 2018/12/15.
+//  Copyright © 2018 Shinji Kurosawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
